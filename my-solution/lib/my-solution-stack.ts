43b84a6380d85cbd1b7433f0497340893533ea4f
Vasyl Herman PC W10 <vasil.german@gmail.com>
2021-02-01 16:01:15 +0200

    update

--- my-solution/lib/my-solution-stack.ts
+++ my-solution/lib/my-solution-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import { CloudFrontToS3 } from "@aws-solutions-constructs/aws-cloudfront-s3";
 
 export class MySolutionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new CloudFrontToS3(this, 'cloudfront-s3', {
+      // ... any properties here for the construct
+    })
   }
 }
